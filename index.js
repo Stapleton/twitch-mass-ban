101aa9e5cd78015c3d5ddd4c4ee84a7d40785d91
OscarXcore <stapletontaylor@hotmail.com>
2017-04-13 14:33:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-require('dotenv').config();
-const _globals_ = require('./lib/globals');
+require('dotenv').config({path: './auth.env'});
+const _tmi = require('tmi.js');
+const _fs = require('fs');
 
-let initPromise1 = new Promise((resolve, reject) => {
-  require('./lib/modStatus');
-  resolve();
+const globals = {
+  script: process.argv[1],
+  channel: process.argv[2],
+  file: process.argv[3],
+  twitch_client_id: 'p04q9em5dsuowcswv6wifxp7p9tgyb'
+}
+
+// Create option object. Contains configuration for _tmi.client()
+const options = {
+  options: { clientId: globals.twitch_client_id, debug: false },
+  connection: { reconnect: true, secure: true },
+  identity: { username: process.env.TWITCH_USERNAME, password: process.env.TWITCH_OAUTH },
+  channels: [ globals.channel ]
+}
+
+// Construct new client with options [Object];
+// Export client out of module as 'client'
+const client = new _tmi.client(options);
+
+// Connect previously constructed client
+client.connect();
+
+client.on('connected', () => {
+  doIt();
 });
-initPromise1.catch(() => { console.log(); });
-initPromise1.then(() => {
-  let initPromise2 = new Promise((resolve, reject) => {
-    require('./lib/twitchConnect');
-    require('./lib/twitchConnect').on('disconnected', (reason) => {
-      reject(reason);
-    });
-    require('./lib/twitchConnect').on('connected', () => {
-      resolve();
-    })
+
+function doIt() {
+  _fs.readFile(globals.file, 'utf8', (error, fileData) => {
+    if (error) throw error;
+
+    let banList = fileData.replace(/\r\n/g, ' ').split(' ');
+    let banQueue = banList;
+
+    if (banQueue.length != 0) {
+      let channel = globals.channel.toString();
+      do {
+        /*_twitch.client.ban(channel, banQueue[0].toString(), `PM ${process.env.TWITCH_USERNAME} about why this user was banned. Automated with Twitch Mass Ban by OscarXcore.`)
+        .then((data) => {
+          console.log(`${data[0]}: ${data[1]} is now banned. Reason: ${data[2]}`);
+        }).catch((error) => {
+          console.log(error);
+        })*/
+        client.say(channel, `/ban ${banQueue[0].toString()} "PM ${process.env.TWITCH_USERNAME} about why this user was banned. Automated with Twitch Mass Ban by OscarXcore."`)
+        console.log(`#${channel}: User #${banQueue.length} | ${banQueue[0].toString()} has been banned from the channel.`);
+        banQueue.shift();
+      } while (banQueue.length != 0);
+    }
+	if (banQueue.length == 0) {
+		console.log('\nFinished. Press CTRL+C to exit.');
+	}
   });
-  initPromise2.catch((reason) => { console.log(reason); });
-  initPromise2.then(() => {
-    let initPromise3 = new Promise((resolve, reject) => {
-      console.log('\nStarting.');
-      require('./lib/fileParsing');
-      resolve();
-    });
-    initPromise3.catch((error) => { console.log(error); });
-    initPromise3.then(() => {
-      if (_globals_.globals.finished == true) {
-        console.log('Done. Exiting.');
-        process.exit(0);
-      }
-    });
-  });
-});
+}
